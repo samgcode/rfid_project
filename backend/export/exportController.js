8d09a90f1b21f79f1d31ea2bed0f7cf367261451
sam gaudet <samgcode@gmail.com>
2020-12-29 10:46:24 -0700

    - updated export code to write to file

--- backend/export/exportController.js
+++ backend/export/exportController.js
@@ -1,4 +1,6 @@
 const moment = require('moment');
+const fs = require('fs');
+const stringify = require('csv-stringify');
 class ExportController {
     constructor(serviceLocator) {
         this._exportService = serviceLocator.services.exportService;
@@ -7,6 +9,7 @@ class ExportController {
     async getTimes(ctx) {
         try {
             const times = await this._exportService.getTimes();
+            this.writeData(times);
             ctx.body = times;
         } catch(err) {
             console.log(err);    
@@ -19,10 +22,28 @@ class ExportController {
             const start = moment.utc(startDate).toDate();
             const end = moment.utc(endDate).toDate();
             const times = await this._exportService.getTimesByDate(start, end);
+            this.writeData(times);
             ctx.body = times;
         } catch(err) {
             console.log(err);    
         }
+    }
+
+    async writeData(input) {
+        input.map(time => {
+            time.timeIn = moment(time.timeIn).toString();
+            time.timeOut = moment(time.timeOut).toString();
+            return time;
+        });
+        stringify(input, {
+            header: true
+        }, function(err,data) {
+            if(err){ throw (err) }
+            const fileName = 'output.csv';
+            fs.writeFile(`./output/${fileName}`, data, {flag: 'w'}, function(err) {
+                if(err) { throw err }
+            });
+        })
     }
 }
 
