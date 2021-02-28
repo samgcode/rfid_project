9de0258148fb78269cb0ee77040f308d35973236
Sam g <samgcode@gmail.com>
2021-02-28 14:50:28 -0700

    - fixed interval on goodby and thanks pages - removed some unnecesary code

--- common/logger/logger.js
+++ common/logger/logger.js
@@ -7,56 +7,11 @@ const { combine, timestamp, printf } = format;
 const config = require('../config');
 const options = config.loggerOptions;
 
-const logger = winston.createLogger({
-    class: '',
-    format: combine(
-        format.errors({ stack: true }),
-        timestamp(),
-        printf(({ level, message, timestamp, stack }) => {
-            return JSON.stringify({
-                class: this.class,
-                timestamp,
-                level,
-                message,
-                stack
-            });
-        })
-    ),
-    transports: [
-        new winston.transports.File(options.file),
-        new winston.transports.Console(options.console)
-    ],
-    exitOnError: false
-});
-
-const scannerLogger = winston.createLogger({
-    format: combine(
-        format.errors({ stack: true }),
-        timestamp(),
-        printf(({ level, message, timestamp, stack }) => {
-            return JSON.stringify({
-                timestamp,
-                level,
-                message,
-                stack
-            });
-        })
-    ),
-    transports: [
-        new winston.transports.File(options.scannerFile),
-    ],
-    exitOnError: false
-});
-
-
-// exports.scannerLogger = scannerLogger;
-// exports.logger = logger;
 exports.createLogger = function(loggerOptions) {
     let transports = [
         new winston.transports.File(options.file),
         new winston.transports.Console(options.console),
     ];
-    console.log(loggerOptions);
     if(loggerOptions.scannerTransport) {
         transports = [
             new winston.transports.File(options.scannerFile),
--- frontend/src/components/Goodbye/Goodbye.vue
+++ frontend/src/components/Goodbye/Goodbye.vue
@@ -23,16 +23,26 @@ export default {
       this.name = this.$route.params.name;
       clearTimeout(this.timeOut);
       this.timeOut = setTimeout(() => {
-        this.$router.push({name: 'Home'});
+        this.returnToHome();
       }, 5000);
     }
   },
   mounted() {
     this.name = this.$route.params.name;
+    clearTimeout(this.timeOut);
     this.timeOut = setTimeout(() => {
-      this.$router.push({name: 'Home'});
+      this.returnToHome();
     }, 5000);
   },
+  methods: {
+    returnToHome: function() {
+      if(this.$router.currentRoute.path === `/goodbye/${this.name}` ||
+         this.$router.currentRoute.path === `/thanks/${this.name}`) 
+      {
+        this.$router.push({name: 'Home'});
+      }
+    }
+  }
 }
 </script>
 
